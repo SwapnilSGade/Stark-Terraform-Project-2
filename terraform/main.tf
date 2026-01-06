bae694a8db640fe6179e1b86027dd113fbf5dbbe
SwapnilSGade <swapnilgade100@gmail.com>
2026-01-06 16:27:36 +0530

    s

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,3 @@
-########################################
-# Local values
-########################################
 locals {
   tags = {
     Project     = var.project_name
@@ -9,52 +6,20 @@ locals {
   }
 }
 
-########################################
-# AWS Module
-########################################
-module "aws" {
-  source                     = "./aws"
-
-  # Core project settings
-  project_name               = var.project_name
-  aws_region                 = var.aws_region
-
-  # S3 source bucket
-  source_s3_bucket           = var.source_s3_bucket
-  source_s3_prefix           = var.source_s3_prefix
-
-  # Lambda + EventBridge
-  schedule_expression        = var.schedule_expression
-  lambda_timeout             = var.lambda_timeout
-  lambda_memory_mb           = var.lambda_memory_mb
-  lambda_reserved_concurrency = var.lambda_reserved_concurrency
-
-  # Secrets + KMS
-  azure_secret_name          = var.azure_secret_name
-  kms_key_arn                = var.kms_key_arn
-
-  # Azure target storage (needed by Lambda sync logic)
-  azure_storage_account_name = var.azure_storage_account_name
-  azure_container_name       = var.azure_container_name
-
-  # Tags (optional, if your AWS module supports them)
-  tags                       = local.tags
+# Include modules/files
+# AWS
+module "aws_iam" {
+  source = "./aws"
 }
 
-########################################
-# Azure Module
-########################################
+# Azure resources
 module "azure" {
-  source                     = "./azure"
-
-  # Resource group + location
-  azure_rg_name              = var.azure_rg_name
-  azure_location             = var.azure_location
-
-  # Storage account + container
-  azure_storage_account_name = var.azure_storage_account_name
-  azure_container_name       = var.azure_container_name
-
-  # Tags
-  tags                       = local.tags
+  source                      = "./azure"
+  azure_rg_name               = var.azure_rg_name
+  azure_location              = var.azure_location
+  azure_storage_account_name  = var.azure_storage_account_name
+  azure_container_name        = var.azure_container_name
+  tags                        = local.tags
 }
+
+# Lambda + EventBridge (defined in aws/*.tf referencing variables directly)
